d1e99c1da06dcdec0147b7bfc9dbdff8f084d188
Shindano Singi <shindanosingi@sekur.com>
2023-11-12 15:49:10 -0500

    Education completed

--- server/routes/educationsRoutes.js
+++ server/routes/educationsRoutes.js
@@ -23,6 +23,7 @@ router.post("/create-education", authMiddleware, async (req, res) => {
 
             // Create new education
             const newEducation = new Education(req.body);
+            newEducation.user = userId;
             await newEducation.save();
 
             // Update user's education
@@ -40,6 +41,104 @@ router.post("/create-education", authMiddleware, async (req, res) => {
                   message: error.message,
                   success: false
             });
+        }
+});
+
+// Get all educations
+router.get("/get-educations", authMiddleware, async (req, res) => {
+      try {
+            const educations = await Education.find({user: req.body.userId});
+            console.log(educations);
+
+
+            return res.send({
+                  message: "Educations fetched successfully",
+                  success: true,
+                  educations
+            });
+      } catch (error) {
+            return res.send({
+                  message: error.message,
+                  success: false
+            });
+      }
+});
+
+// Get a single education
+router.get("/get-education/:id", authMiddleware, async (req, res) => {
+
+      try {
+            const education = await Education.findById(req.params.id);
+
+            return res.send({
+                  message: "Education fetched successfully",
+                  success: true,
+                  education
+            });
+      } catch (error) {
+            return res.send({
+                  message: error.message,
+                  success: false
+            });
+      }
+});
+
+// Update a single education
+router.put("/update-education/:id", authMiddleware, async (req, res) => {
+
+        try {
+                const updatedEducation = await Education.findByIdAndUpdate(req.params.id, req.body, {new: true});
+                updatedEducation.save();
+
+                if (!updatedEducation){
+                        return res.send({
+                            message: "Education not found",
+                            success: false
+                        });
+                }
+
+                return res.send({
+                    message: "Education fetched successfully",
+                    success: true,
+                    education: updatedEducation
+                });
+        } catch (error) {
+                return res.send({
+                    message: error.message,
+                    success: false
+                });
+        }
+});
+
+// Delete a single education
+router.delete("/delete-education/:id", authMiddleware, async (req, res) => {
+
+        try {
+                const deletedEducation = await Education.findByIdAndDelete(req.params.id);
+
+                if (!deletedEducation){
+                        return res.send({
+                            message: "Education not found",
+                            success: false
+                        });
+                }
+
+                // Remove a single education from the user's education array
+                const user = await User.findById(deletedEducation.user);
+                user.education.pull(deletedEducation.id);
+                user.education.filter((id) => deletedEducation.id !== id);
+
+                await user.save();
+
+                return res.send({
+                    message: "Education deleted successfully",
+                    success: true,
+                });
+        } catch (error) {
+                return res.send({
+                    message: error.message,
+                    success: false
+                });
         }
 });
 
