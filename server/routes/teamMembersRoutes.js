e9bc47f90e7acb25b691542b7cf86a7199480f1d
Shindano Singi <shindanosingi@sekur.com>
2023-11-18 10:25:24 -0500

    user can add team member to a project

--- server/routes/teamMembersRoutes.js
+++ server/routes/teamMembersRoutes.js
@@ -1,51 +1,51 @@
 const User = require("../models/userModel");
 const Project = require("../models/projectModel");
-const teamMember = require("../models/teamMemberModel");
+const TeamMember = require("../models/teamMemberModel");
 
 const router = require("express").Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const cloudinary = require("cloudinary");
 
-// Create a new teamMember
-router.post("/create-teamMember", authMiddleware, async (req, res) => {
-      try {
-            const { userId } = req.body;
+// Create a new Team member
+router.post('/projects/:projectId/add-team-member', authMiddleware, async (req, res) => {
+    try {
+        const { projectId } = req.params;
+        const { userId } = req.body;
 
-            // Check if user already exists
-            const user = await User.findById(userId);
+        const project = await Project.findById(projectId);
+        const user = await User.findById(userId);
 
-
-            // if (!user) {
-            //       return res.send({
-            //             status: "error",
-            //             message: "User does not exist"
-            //       });
-            // }
-
-            const member = await teamMember.create(req.body);
-            member.user = userId;
-            await member.save();
-
-            console.log(user.projects);
-
-            user.projects.teamMembers.push(member._id);
-
-            await user.save();
-
-            console.log(user);
-
+        if (!project || !user) {
             return res.send({
-                  success: true,
-                  message: "teamMember successfully created",
-                  data: member
-            });
-      } catch (error) {
+                message: "Project or User not found",
+                success: false
+            })
+        }
+
+        const teamMember = await TeamMember.create(req.body);
+
+        const updatedProject = await Project.findByIdAndUpdate(
+            projectId,
+            { $push: { teamMembers: teamMember._id } },
+            { new: true }
+        ).populate("teamMembers");
+
+        if(!updatedProject) {
             return res.send({
-                  success: false,
-                  message: "Error in finding user",
-                  data: error.message
-            });
-      }
+                message: "Project not found",
+                success: false
+            })
+        }
+
+        res.send({
+            message: "Team member added successfully",
+            success: true,
+            data: updatedProject
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 module.exports = router;
