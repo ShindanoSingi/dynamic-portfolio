ea3e6dbbe215a3307679781258906fc4b8f9043d
Shindano Singi <shindanosingi@sekur.com>
2024-01-26 23:34:19 -0500

    Fixed side menu

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -24,6 +24,7 @@ const Home = () => {
                         alt=""
                   /> : <Loader/>
                   }
+                  
                   <Footer />
                         </div>
                   }
